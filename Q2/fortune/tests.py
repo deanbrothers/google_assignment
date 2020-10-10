099ac9653046b6cf7ecc0009ba2ccc88af2180bb
Rahul <rk3918@gmail.com>
2020-10-11 00:00:50 +0530

    Added Test Cases

--- Q2/fortune/tests.py
+++ Q2/fortune/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from fortune.models import Company, FortuneCookie
+
+class CompanyTestCase(TestCase):
+
+    def get_company_with_id(self):
+        """Get the company with ID"""
+        Company.objects.create(name='RkFortune')
+        company_obj = Company.objects.get(id=1)
+        self.assertTrue(isinstance(company_obj, Company))
+
+
+class FortuneCookieTestCase(TestCase):
+
+    def get_fortune_with_name(self):
+        """Test fortune data"""
+        company_obj = Company.objects.create(name='RkFortune')
+        FortuneCookie.objects.create(fortune_option='A miss is as good as a mile.', company_id=company_obj.id)
+        cookie_obj = FortuneCookie.objects.get(fortune_option='A miss is as good as a mile.')
+        self.assertTrue(isinstance(cookie_obj, FortuneCookie))
